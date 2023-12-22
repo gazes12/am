087cf3c3cbcdc9f08a3719ad8e82d285de64fe1d
gazes12 <khozhainovdmitro@gmail.com>
2023-12-22 21:28:52 +0200

    .

---
+++ js/preloader.js
@@ -0,0 +1,13 @@
+
+// $('body').addClass('lock');
+
+// $(window).on('load', function(){
+// 		setTimeout(() =>{
+// 		$('.preloader').addClass('hidden');
+// 		$('body').removeClass('lock');
+// 	}, 2000);
+// });
+
+
+$('body').removeClass('lock');
+$('.preloader').addClass('hidden');
